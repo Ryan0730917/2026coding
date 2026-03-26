ccd03659933cd2349ee1758de8ac210d5073a6ae
Ryan0730917 <lij71610@gmail.com>
2026-03-26 16:30:33 +0800

    week05

---
+++ week05/week05-4.py
@@ -0,0 +1,17 @@
+#week05-4.py leetcode 3546
+class Solution:
+    def canPartitionGrid(self, grid: List[List[int]]) -> bool:
+        total = sum( [sum(row) for row in grid] )
+        preSum = 0
+        for row in grid:
+            preSum += sum(row)
+            if preSum == total - preSum:
+                return True
+        return False
+
+        preSum = 0
+        for col in zip(*grid):
+            preSum += sum(col)
+            if preSum == total- preSum:
+                return True
+        return False
